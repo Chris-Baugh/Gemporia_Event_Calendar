f44e96b1103129a291f6f882f29cc3f8c1142210
Chris-Baugh <baugh.cs@gmail.com>
2022-05-25 13:42:31 +0100

    New model "User" created using IdentityUser. New User pages scaffolded from User Identity for login, register, forgot passwrd etc. Login Partial updated to focus on User Class as the SignInManager and UserManager injections

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gemporia_Event_Calendar.Data;
+using Gemporia_Event_Calendar.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Gemporia_Event_Calendar.Areas.Identity.IdentityHostingStartup))]
+namespace Gemporia_Event_Calendar.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using Gemporia_Event_Calendar.Data;
+using Gemporia_Event_Calendar.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,7 +31,7 @@ namespace Gemporia_Event_Calendar
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
